34d9650f40019366a5f577e11a9cd6edb21c246e
LORD-RIAN <lordjulian.s.rian@gmail.com>
2025-10-15 19:07:12 +0200

    struct

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 # main.py
+
+
+### Subdomain Discovery
+
+
+
+### Subdomain Discovery
+
+
+
+### IP Discovery
+
+
+
+### IP Discovery
+
+
+### Paths - Fuzzing
+
+
+### Paths - Fuzzing
+
+
+### Scans / Nuclei & similar
+
+
+
+### Scans / Nuclei & similar
+
+### Port Scanning
+
+
+
+### Port Scanning
